c8a0cbe14d582427af035a7b9e49c4705e63f70b
Evgenii Sergunin <mckrei@ya.ru>
2023-05-18 12:59:06 +0300

    add task3

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
+import datetime
+
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///urls.db'
+
+db = SQLAlchemy(app)
+
+
+class URLmodel(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    original_url = db.Column(db.String(255))
+    short = db.Column(db.String(6), unique=True)
+    visits = db.Column(db.Integer, default=0)
+    created_at = db.Column(db.DateTime, default=datetime.datetime.now)
+
+
+db.create_all()
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -10,7 +27,8 @@ def index():
 
 @app.route('/urls', methods=['GET'])
 def urls():
-    return render_template('urls.html')
+    urls = URLmodel.query.all()
+    return render_template('urls.html', urls=urls)
 
 
 @app.route('/<string:short>', methods=['GET'])
